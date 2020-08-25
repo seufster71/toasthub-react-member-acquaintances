f33e645e42533a9139db3cb2801fc47b6fed536f
Edward Seufert <edwardseufert@Edwards-MBP.home>
2020-08-25 07:36:05 -0400

    menu update

--- acquaintances-reducer.js
+++ acquaintances-reducer.js
@@ -4,7 +4,21 @@ export default function acquaintanceReducer(state = {}, action) {
   let myState = {};
   switch(action.type) {
     case 'LOAD_INIT_ACQUAINTANCE': {
-      return processInit(state,action);
+    	if (action.responseJson != null && action.responseJson.params != null) {
+    	    return Object.assign({}, state, {
+    	      prefForms: Object.assign({}, state.prefForms, reducerUtils.getPrefForms(action)),
+    	      prefTexts: Object.assign({}, state.prefTexts, reducerUtils.getPrefTexts(action)),
+    	      prefLabels: Object.assign({}, state.prefLabels, reducerUtils.getPrefLabels(action)),
+    	      prefOptions: Object.assign({}, state.prefOptions, reducerUtils.getPrefOptions(action)),
+    	      columns: reducerUtils.getColumns(action),
+    	      itemCount: reducerUtils.getItemCount(action),
+    	      items: reducerUtils.getItems(action),
+    	      pageLimit: reducerUtils.getPageLimit(action),
+    	      pageStart: reducerUtils.getPageStart(action)
+    	    });
+    	  } else {
+    	    return state;
+    	  }
     }
     case 'SAVE_ACQUAINTANCE': {
       return state;
@@ -13,21 +27,3 @@ export default function acquaintanceReducer(state = {}, action) {
       return state;
   }
 }
-
-const processInit = (state,action) => {
-  if (action.responseJson != null && action.responseJson.params != null) {
-    return Object.assign({}, state, {
-      prefForms: Object.assign({}, state.prefForms, reducerUtils.getPrefForms(action)),
-      prefTexts: Object.assign({}, state.prefTexts, reducerUtils.getPrefTexts(action)),
-      prefLabels: Object.assign({}, state.prefLabels, reducerUtils.getPrefLabels(action)),
-      prefOptions: Object.assign({}, state.prefOptions, reducerUtils.getPrefOptions(action)),
-      columns: reducerUtils.getColumns(action),
-      itemCount: reducerUtils.getItemCount(action),
-      items: reducerUtils.getItems(action),
-      pageLimit: reducerUtils.getPageLimit(action),
-      pageStart: reducerUtils.getPageStart(action)
-    });
-  } else {
-    return state;
-  }
-};
